4bfe484173cdf23a114eb7f19f008d79f297d4a9
Ivan Schaller <ivan@schaller.sh>
2025-05-11 20:45:30 +0200

    test new mv
    Signed-off-by: Ivan Schaller <ivan@schaller.sh>

--- lib/utils.bash
+++ lib/utils.bash
@@ -124,7 +124,8 @@ function install_version() {
 
     mkdir -p "${install_path}"
 
-    mv -f "${download_path}/python" "${install_path}"
+    mv -f "${download_path}/python/"* "${install_path}/"
+    chmod +x "${install_path}"/bin/*
 
     if [[ ! -x "${install_path}/bin/${TOOL_NAME}" ]]; then
         #rm -rf "${install_path}"
